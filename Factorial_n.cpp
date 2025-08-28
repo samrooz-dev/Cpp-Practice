e2cc22c3f262630f2d7450c1dccab12d932e0548
samrooz-dev <samroozab@gmail.com>
2025-08-28 10:52:28 +0500

    Add files via upload

---
+++ Factorial_n.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cout<<"Enter the number for factorial: ";
+    cin>>n;
+
+    long long fact = 1;
+
+    for (int i = 1; i <=n; i++)
+    {
+        fact *= i;
+    }
+
+    cout<<"The factorial of "<<n<<" is: "<<fact<<endl;
+    
+    return 0;
+}
